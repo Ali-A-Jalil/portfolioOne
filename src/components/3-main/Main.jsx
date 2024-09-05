6efc00338bec1c26be2d87d58ba2bf56a093e828
Ali-A-Jalil <aliabdelgalil101@gmail.com>
2024-09-06 01:32:36 +0300

    Last updated on 2022-12-13 at 16:30:00

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,72 +1,115 @@
 
+import { useState } from "react";
 import "./main.css";
+
 
 const Main = () => {
 
-  // const products = [
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgOne,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgTwo,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgThree,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgFour,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgFive,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgSix,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgSeven,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgEight,
-  //   },
-  //   {
-  //     title: "My Portfolio",
-  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
-  //     imgUrl: imgNine,
-  //   }
-  // ];
+
+
+  const [type, setType] = useState("all");
+  
+
+  const products = [
+    {
+      id: 1,
+      title: "My Portfolio",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/one.jpg",
+      category: "React-JS",
+
+    },
+    {
+      id: 2,
+      title: "Car Store",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/two.jpg",
+      category: "React-JS",
+    },
+    {
+      id: 3,
+      title: "E-commerce Website", 
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/three.jpg",
+      category: "JavaScript",
+    },
+    {
+      id: 4,
+      title: "Todo App",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/four.jpg",
+      category: "Python",
+    },
+    {
+      id: 5,
+      title: "Gym App",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/five.jpg",
+      category: "JavaScript",
+    },
+    {
+      id: 6,
+      title: "School App",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/six.jpg",
+      category: "Python",
+    },
+    {
+      id: 7,
+      title: "Our Mission",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/seven.jpg",
+      category: "Python",
+    },
+    {
+      id: 8,
+      title: "Furniture Store",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/eight.jpg",
+      category: "HTML & CSS",
+    },
+    {
+      id: 9,
+      title: "Cloud Hosting",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusantium, quae, voluptates, eum cumque autem quidem exercitationem iste numquam quia doloremque.",
+      imgUrl: "./public/nine.jpg",
+      category: "HTML & CSS",
+    }
+  ];
+
+
+  const filteredProducts = type==="all" ? products : products.filter((product) => product.category === type);
+
+
+
+
+
 
 
   return (
     <section className="main">
       <div className="left-section">
-        <button className="btn-card active">All Projects</button>
-        <button className="btn-card">HTML & CSS</button>
-        <button className="btn-card">JavaScript</button>
-        <button className="btn-card">React-JS </button>
-        <button className="btn-card">Python</button>
+
+
+        <button className={`btn-card ${type === "all" ? "active" : ""}`} onClick={() => setType("all")} >All Projects</button>
+
+        <button className={`btn-card ${type === "HTML & CSS" ? "active" : ""}`} onClick={() => setType("HTML & CSS")} >HTML & CSS</button>
+
+
+        <button className={`btn-card ${type === "JavaScript" ? "active" : ""}`} onClick={() => setType("JavaScript")} >JavaScript</button>
+
+        <button className={`btn-card ${type === "React-JS" ? "active" : ""}`} onClick={() => setType("React-JS")} >React-JS </button>
+
+
+        <button   className={`btn-card ${type === "Python" ? "active" : ""}`} onClick={() => setType("Python")} >Python</button>
       </div>
+
       <div className="right-section">
 
-        {/* {
-          products.map((product) => (
-            <article key={product.title} className="card">
-              <img className="image-card" src={product.imgUrl} alt="" />
+        {
+          filteredProducts.map((product, index) => (
+            <article key={index} className="card">
+              <img className="image-card" src={product.imgUrl} alt={product.title} />
               <div className="box">
                 <h1 className="title-card">{product.title}</h1>
                 <p className="description-card">{product.description}</p>
@@ -83,31 +126,8 @@ const Main = () => {
               </div>
             </article>
           ))
-        } */}
+        }
 
-        {[...Array(9)].map((_, index) => (
-                  <article key={index} className="card">
-                  <img className="image-card" src="" alt="" />
-                  <div className="box">
-                    <h1 className="title-card">Title</h1>
-                    <p className="description-card">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-                      accusantium, quae, voluptates, eum cumque autem quidem
-                      exercitationem iste numquam quia doloremque.
-                    </p>
-                    <div className="icon-card ">
-                      <div className="icons-box">
-                        <a className="icons icon-link" />
-                        <a className="icons icon-github" />
-                      </div>
-                      <a className="more-card" href="" >
-                        more 
-                        < i className="icon-arrow-right arrow"/>
-                      </a>
-                    </div>
-                  </div>
-                </article>
-              ))}
 
       </div>
     </section>
